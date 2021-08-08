e5e8f2e7c15816b988b6596b265a8d8e5689292d
ifeegoo <root@ifeegoo.com>
2021-08-08 22:20:22 +0800

    Build the VERSION class.

---
+++ src/com/ifeegoo/app/backgitlabup/tools/BackGitLabUp.java
@@ -0,0 +1,12 @@
+package com.ifeegoo.app.backgitlabup.tools;
+
+public class BackGitLabUp {
+    public static final class VERSION {
+        public static final int BUILD = 1;
+        public static final int MAJOR = 0;
+        public static final int MINOR = 1;
+        public static final int PATCH = 0;
+        private static final String STATUS = "";
+        public static final String NAME = String.valueOf(MAJOR) + "." + String.valueOf(MINOR) + "." + String.valueOf(PATCH) + "-" + STATUS;
+    }
+}
