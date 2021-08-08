ccde2bb8c726835d4416d980291a39851aa5180c
ifeegoo <root@ifeegoo.com>
2021-08-08 22:27:51 +0800

    Modify the STATUS from private to public and add some annotations.

--- src/com/ifeegoo/app/backgitlabup/tools/BackGitLabUp.java
+++ src/com/ifeegoo/app/backgitlabup/tools/BackGitLabUp.java
@@ -1,12 +1,54 @@
 package com.ifeegoo.app.backgitlabup.tools;
 
 public class BackGitLabUp {
+
+    /**
+     * This is a helper class for users to get the version of BackGitLabUp.
+     *
+     * @since 0.1.0
+     */
     public static final class VERSION {
+
+        /**
+         * The build number of BackGitLabUp.
+         *
+         * @since 0.1.0
+         */
         public static final int BUILD = 1;
+
+        /**
+         * The major number of the version name of BackGitLabUp.
+         *
+         * @since 0.1.0
+         */
         public static final int MAJOR = 0;
+
+        /**
+         * The minor number of the version name of BackGitLabUp.
+         *
+         * @since 0.1.0
+         */
         public static final int MINOR = 1;
+
+        /**
+         * The patch number of the version name of BackGitLabUp.
+         *
+         * @since 0.1.0
+         */
         public static final int PATCH = 0;
-        private static final String STATUS = "";
+
+        /**
+         * The status of the version name of BackGitLabUp.
+         *
+         * @since 0.1.0
+         */
+        public static final String STATUS = "";
+
+        /**
+         * The version name of BackGitLabUp.
+         *
+         * @since 0.1.0
+         */
         public static final String NAME = String.valueOf(MAJOR) + "." + String.valueOf(MINOR) + "." + String.valueOf(PATCH) + "-" + STATUS;
     }
 }
