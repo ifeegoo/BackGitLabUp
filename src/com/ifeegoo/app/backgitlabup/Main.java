6c3fe3d33811d7f646c0a1e24b4e8712a5b27d96
ifeegoo <root@ifeegoo.com>
2021-08-09 12:04:53 +0800

    Add the java -jar parameter -help.

--- src/com/ifeegoo/app/backgitlabup/Main.java
+++ src/com/ifeegoo/app/backgitlabup/Main.java
@@ -1,8 +1,41 @@
 package com.ifeegoo.app.backgitlabup;
 
+import com.ifeegoo.app.backgitlabup.tools.BackGitLabUp;
+
 public class Main {
 
+    private static String sParameter;
+    private static final String NAME_TOOL = "BackGitLabUp";
+
     public static void main(String[] args) {
-        System.out.println("This is a test");
+        if ((args != null) && (args.length >= 1))
+        {
+            sParameter = args[0];
+
+            switch (args.length)
+            {
+                case 1:
+                {
+                    switch (sParameter)
+                    {
+                        case "-help":
+                        {
+                            System.out.println(NAME_TOOL + ": " + BackGitLabUp.VERSION.NAME + "(" + BackGitLabUp.VERSION.BUILD +")");
+                            System.out.println(NAME_TOOL + " is a simple & useful GitLab backup tool.");
+                            System.out.println();
+                            System.out.println("Usage:");
+                            System.out.println();
+                            System.out.println("1. Use [-help] to get how to use BackGitLabUp.");
+                            System.out.println("java -jar BackGitLabUp.jar -help");
+                            System.out.println();
+                            System.out.print("2. Use [-v] or [-ver] or [-version] to get the Version of BackGitLabUp.");
+                            System.out.println("java -jar BackGitLabUp.jar -v");
+                            System.out.println("java -jar BackGitLabUp.jar -ver");
+                            System.out.println("java -jar BackGitLabUp.jar -version");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
