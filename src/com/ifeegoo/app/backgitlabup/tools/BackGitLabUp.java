e3ea59691a4dd5da90865bd58803c40ef12a9830
ifeegoo <root@ifeegoo.com>
2021-12-05 21:58:45 +0800

    Fetch git repositories data from GitLab.

--- src/com/ifeegoo/app/backgitlabup/tools/BackGitLabUp.java
+++ src/com/ifeegoo/app/backgitlabup/tools/BackGitLabUp.java
@@ -1,5 +1,14 @@
 package com.ifeegoo.app.backgitlabup.tools;
 
+import com.alibaba.fastjson.JSON;
+import com.ifeegoo.app.backgitlabup.beans.ProjectsItem;
+import com.ifeegoo.app.backgitlabup.beans.Response;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BackGitLabUp {
 
     private static BackGitLabUp sBackGitLabUp;
@@ -8,6 +17,9 @@ public class BackGitLabUp {
     private String mGitLabGroupID = "";
     private String mGitLabPrivateToken = "";
     private String mGitLabRepositoryAccessType = "";
+
+    private List<String> mTotalRepositoriesURLs = new ArrayList<String>();
+    private List<String> mTotalRepositoriesDirectories = new ArrayList<String>();
 
     private BackGitLabUp()
     {
@@ -58,13 +70,72 @@ public class BackGitLabUp {
         return sBackGitLabUp;
     }
 
-    public BackGitLabUp start()
+    public void start()
     {
         String apiURL = this.mGitLabURL + "/api/" + this.mGitLabAPIVersion + "/groups/" + this.mGitLabGroupID + "?private_token=" + this.mGitLabPrivateToken;
 
+        try
+        {
+            run(apiURL);
 
-        return sBackGitLabUp;
+            Response response = JSON.parseObject(run(apiURL), Response.class);
+
+
+            List<ProjectsItem> projects = response.getProjects();
+
+            if ((projects == null) || (projects.size() == 0))
+            {
+                return;
+            }
+
+            for (int i = 0; i < projects.size(); i++)
+            {
+                switch (this.mGitLabRepositoryAccessType)
+                {
+                    case "SSH":
+                        this.mTotalRepositoriesURLs.add(projects.get(i).getSshUrlToRepo());
+                        break;
+                    case "HTTP":
+                        this.mTotalRepositoriesURLs.add(projects.get(i).getHttpUrlToRepo());
+                        break;
+                }
+
+                this.mTotalRepositoriesDirectories.add(projects.get(i).getPathWithNamespace());
+            }
+
+
+        } catch (Exception e)
+        {
+
+        }
     }
+
+    static OkHttpClient client = new OkHttpClient();
+
+    private String run(String url) throws Exception
+    {
+        Request request;
+        okhttp3.Response response = null;
+
+        try
+        {
+            request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            response = client.newCall(request).execute();
+        } catch (Exception e)
+        {
+
+        }
+
+        String result = response.body().string();
+
+        System.out.println(result);
+
+        return result;
+    }
+
 
     /**
      * This is a helper class for users to get the version of BackGitLabUp.
